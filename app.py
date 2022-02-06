8cc033b93b73152d48de1f9397efa964343876c0
fergus <>
2022-02-06 21:17:37 +0000

    old server config

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ import dash_bootstrap_components as dbc
 
   
 # Server side configs
-#server = flask.Flask(__name__) # define flask app.server
+server = flask.Flask(__name__) # define flask app.server
 app = dash.Dash(__name__,
                 suppress_callback_exceptions=True,
+                server=server,
                 external_stylesheets=["assets\classic.css"])#[dbc.themes.SOLAR]) # NOTE: Bootstrap theme SANDSTONE does not operate well with the sidebar menu and navbar menu
-server = app.server
+#server = app.server
 app.title = "Demand-Response dashboard"
