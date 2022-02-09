e9569961d2fa86cc344f0ddf67d91a2c0d6bb9c7
fergus <>
2022-02-09 16:08:22 +0000

    properly fixed \ to /

--- callbacks.py
+++ callbacks.py
@@ -26,10 +26,10 @@ import tensorflow as tf
 
 
 MELTEMI_DF = pd.read_csv('data/MELTEMI_DF_dummy.csv', parse_dates=['date_time'], sep=';')
-MELTEMI_DD = pd.read_csv('app/data\MELTEMI_DD_dummy.csv', parse_dates=['date_time'], sep=";")
-# ANSRO_DF = pd.read_csv('data\ANSRO_DF_dummy.csv', parse_dates=['date_time'], sep=';')
-# ANSRO_DD = pd.read_csv('data\ANSRO_DD_dummy.csv', parse_dates=['date_time'], sep=";")
-# SOLAR_DUMMY = pd.read_excel('data\solar_Data\dummy_data.xlsx')
+MELTEMI_DD = pd.read_csv('app/data/MELTEMI_DD_dummy.csv', parse_dates=['date_time'], sep=";")
+ANSRO_DF = pd.read_csv('data/ANSRO_DF_dummy.csv', parse_dates=['date_time'], sep=';')
+ANSRO_DD = pd.read_csv('data/ANSRO_DD_dummy.csv', parse_dates=['date_time'], sep=";")
+SOLAR_DUMMY = pd.read_excel('data/solar_Data/dummy_data.xlsx')
 
 #
 # @app.callback(
