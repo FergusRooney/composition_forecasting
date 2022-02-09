1e69f55423a20b8b4c39ea8eeefe59f6db97f14f
fergus <>
2022-02-09 15:26:23 +0000

    no callback

--- index.py
+++ index.py
@@ -5,18 +5,9 @@ import dash_bootstrap_components as dbc
 from dash import dcc
 from dash.dependencies import Input, Output, State
 import gunicorn
+from app import app, server
 from layouts import load_decomposition_layout, welcome_layout, about_project_layout, load_forecast_layout, upload_layout
 import callbacks
-
-
-# Server side configs
-server = flask.Flask(__name__) # define flask app.server
-app = dash.Dash(__name__,
-                suppress_callback_exceptions=True,
-                server=server,
-                external_stylesheets=["assets\classic.css"])#[dbc.themes.SOLAR]) # NOTE: Bootstrap theme SANDSTONE does not operate well with the sidebar menu and navbar menu
-server = app.server
-app.title = "Demand-Response dashboard"
 
 
 
